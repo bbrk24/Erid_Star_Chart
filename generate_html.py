0f2fd9f8f8797206e8881f00104476e1e4eb83c2
Riley Baker <bbrk24@gmail.com>
2026-09-10 21:18:43 -0400

    Saving work on constellation views

--- generate_html.py
+++ generate_html.py
@@ -2,7 +2,7 @@
 
 import csv
 from dataclasses import dataclass
-import math
+from math import pi, sin, cos, tan, acos, atan2
 
 
 @dataclass
@@ -21,13 +21,11 @@ def magnitude_to_opacity(magnitude: float) -> float:
 
 
 def get_coordinates(latitude: float, longitude: float):
-    """Takes degrees (0-360, 0-90) and returns cartesian coordinates 0-1"""
-    phi = math.pi * (90.0 + latitude) / 180.0
-    theta = math.pi * longitude / 180.0
-    radius = 1.0 / math.tan(phi / 2.0)
-    x = radius * math.cos(theta)
-    y = radius * math.sin(theta)
-    return ((x + 1.0) / 2.0, (y + 1.0) / 2.0)
+    """Takes degrees (0..360, 0..90) and returns cartesian coordinates -1..1"""
+    phi = pi * (90.0 + latitude) / 180.0
+    theta = pi * longitude / 180.0
+    radius = 1.0 / tan(phi / 2.0)
+    return (radius * cos(theta), radius * sin(theta))
 
 
 def get_stars():
@@ -65,26 +63,58 @@ def get_stars():
 def main():
     north_hemisphere_html = ""
     south_hemisphere_html = ""
+    orion_html = ""
+
+    orion_limit_x, orion_limit_y = get_coordinates(
+        90.0 - 20.22992832414391, 180.0 * atan2(31, 26) / pi
+    )
 
     for star in sorted(get_stars(), key=lambda s: -s.magnitude):
         opacity = magnitude_to_opacity(star.magnitude)
 
         if star.declination >= 0.0:
             x, y = get_coordinates(star.declination, star.longitude)
-            star_html = f'<div class="star" title="{star.name}" style="left: {x * 100.0}%; top: {y * 100.0}%; opacity: {opacity};"></div>'
+            star_html = f'<div class="star" title="{star.name}" style="left: {(x + 1.0) * 50.0}%; top: {(y + 1.0) * 50.0}%; opacity: {opacity};"></div>'
             north_hemisphere_html += star_html
 
         if star.declination <= 0.0:
             x, y = get_coordinates(-star.declination, -star.longitude)
-            star_html = f'<div class="star" title="{star.name}" style="left: {x * 100.0}%; top: {y * 100.0}%; opacity: {opacity};"></div>'
+            star_html = f'<div class="star" title="{star.name}" style="left: {(x + 1.0) * 50.0}%; top: {(y + 1.0) * 50.0}%; opacity: {opacity};"></div>'
             south_hemisphere_html += star_html
+
+        # center RA on +90, so that changing declination is an X-axis rotation
+        orion_phi = pi * (star.longitude + 3.0) / 180.0
+        # center declination on 90
+        orion_theta = pi * (90.0 - star.declination) / 180.0
+        orion_alpha = pi * 101.5 / 180.0
+        # https://stackoverflow.com/a/5279478/6253337
+        orion_theta_prime = acos(
+            sin(orion_theta) * sin(orion_phi) * sin(orion_alpha)
+            + cos(orion_theta) * cos(orion_alpha)
+        )
+        if orion_theta_prime <= pi / 2.0:
+            orion_y_prime = sin(orion_theta) * sin(orion_phi) * cos(orion_alpha) - cos(
+                orion_theta
+            ) * sin(orion_alpha)
+            orion_x_prime = sin(orion_theta) * cos(orion_phi)
+            orion_phi_prime = atan2(orion_y_prime, orion_x_prime)
+            x, y = get_coordinates(
+                90.0 - 180.0 * orion_theta_prime / pi, 180.0 * orion_phi_prime / pi
+            )
+
+            if abs(x) <= abs(orion_limit_x) and abs(y) <= abs(orion_limit_y):
+                html_left = (x / abs(orion_limit_x) + 1.0) * 50.0
+                html_top = (y / abs(orion_limit_y) + 1.0) * 50.0
+                orion_html += f'<div class="star" title="{star.name}" style="left: {html_left}%; top: {html_top}%; opacity: {opacity};"></div>'
 
     template = ""
     with open("template.html", "r") as templatefile:
         template = templatefile.read()
 
-    full_html = template.replace("{{NorthHemisphere}}", north_hemisphere_html).replace(
-        "{{SouthHemisphere}}", south_hemisphere_html
+    full_html = (
+        template.replace("{{NorthHemisphere}}", north_hemisphere_html)
+        .replace("{{SouthHemisphere}}", south_hemisphere_html)
+        .replace("{{Orion}}", orion_html)
     )
 
     with open("map.html", "w") as output:
